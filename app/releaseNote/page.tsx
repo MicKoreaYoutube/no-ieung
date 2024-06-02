dda80cdb0a13a23d11829097d4663c8576fc3a9e
leejunsibal <118183460+MicKoreaYoutube@users.noreply.github.com>
2024-06-02 13:57:04 +0000

    dP!

--- app/releaseNote/page.tsx
+++ app/releaseNote/page.tsx
@@ -3,7 +3,8 @@ import { formatText } from "../../ts/excludeIeung-util"
 export default function ReleaseNotePage() {
 
   const releaseNote = `2024-05-22 beta 1.0 출시
-  2024-05-28 1.0 출시 - 법전, 실시간 번ㅕㄱ 기느 추가, 디자ㅣㄴ 변겨`
+  2024-05-28 1.0 출시 - 법전, 실시간 번ㅕㄱ 기느 추가, 디자ㅣㄴ 변겨
+  2024-06-02 1.0.1 ㅓㅂ데ㅣ트 - 채티바 참ㅕ 바법 서술 추가, 디자ㅣㄴ ㅗㅕㄴ대ㅗㅘ`
 
   return (
     <>
